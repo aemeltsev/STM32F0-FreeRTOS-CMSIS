38d7a0ff822fd50e3489c77ef0dd704f40ff4efe
Tony Emeltsev <aemeltsev@gmail.com>
2026-04-12 18:27:40 +0300

    feat: upgrage CMSIS and FreeRTOS modules

--- lib/CMSIS.mk
+++ lib/CMSIS.mk
@@ -1,13 +1,16 @@
-CMSIS_VERSION = 4.1
+# New version
+CMSIS_VERSION = 5.9.0
 
 MCU_FAMILY_LOWERCASE = $(shell echo $(MCU_FAMILY) | tr '[:upper:]' '[:lower:]')
 MCU_MODEL_FAMILY_LOWERCASE  = $(shell echo $(MCU_MODEL_FAMILY) | tr '[:upper:]' '[:lower:]')
 
 CMSIS = ./lib/CMSIS/$(CMSIS_VERSION)
 
+# The paths remain the same
 CSRC += $(CMSIS)/Device/ST/$(MCU_FAMILY)/Source/Templates/system_$(MCU_FAMILY_LOWERCASE).c
 
 ASMSRC += $(CMSIS)/Device/ST/$(MCU_FAMILY)/Source/Templates/gcc/startup_$(MCU_MODEL_FAMILY_LOWERCASE).s
 
+# IMPORTANT: Add path to Core Include
 INCDIR += $(CMSIS)/Device/ST/$(MCU_FAMILY)/Include \
-           $(CMSIS)/Include
+          $(CMSIS)/Include
